9cc0768dedfb17ed1ff32fb8c5615a6dd8572240
boris <boris.lok@cienet.com>
2023-03-07 09:58:05 +0800

    [Feature] Add a checking to check the date is weekend.

---
+++ src/utils.rs
@@ -0,0 +1,32 @@
+use chrono::{Datelike, Weekday};
+
+pub fn check_date_is_weekend(date: chrono::DateTime<chrono::Local>) -> bool {
+    matches!(date.weekday(), Weekday::Sat | Weekday::Sun)
+}
+
+#[cfg(test)]
+mod test {
+    use super::check_date_is_weekend;
+    use chrono::DateTime;
+
+    #[test]
+    fn it_works_when_checking_the_date_is_weekend() {
+        let date_str = vec![
+            "2023-03-06T00:00:00+08:00", // Mon
+            "2023-03-07T00:00:00+08:00", // Tue
+            "2023-03-08T00:00:00+08:00", // Wed
+            "2023-03-09T00:00:00+08:00", // Thu
+            "2023-03-10T00:00:00+08:00", // Fri
+            "2023-03-11T00:00:00+08:00", // Sat
+            "2023-03-12T00:00:00+08:00", // Sun
+        ];
+        let expected_values = vec![false, false, false, false, false, true, true];
+        for (&s, &expected_value) in date_str.iter().zip(expected_values.iter()) {
+            let date = DateTime::parse_from_rfc3339(s)
+                .unwrap()
+                .with_timezone(&chrono::Local);
+
+            assert_eq!(check_date_is_weekend(date), expected_value);
+        }
+    }
+}
